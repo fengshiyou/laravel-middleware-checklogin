09d1b34880e6373b4fa55b3e1227206527124bdc
fengshiyou <501453944@qq.com>
2016-12-28 09:01:50 +0000

    update

--- src/CheckLoginMiddleware.php
+++ src/CheckLoginMiddleware.php
@@ -1,22 +1,40 @@
 <?php
-namespace Union\MidCheckLogin;
-use Predis;
+namespace laravel\MidCheckLogin;
+use Illuminate\Support\Facades\Redis;
 use Union\Mid\BaseMiddleware;
 
 class CheckLoginMiddleware extends BaseMiddleware
 {
-    public static function test()
+    protected function match($request)
     {
-        $server = array(
-            'host' => '127.0.0.1',
-            'port' => 6379,
-            'database' => 15
-        );
-        $client = new Predis\Client($server);
-        $client->set('foo', 'bar');
-        $value = $client->get('foo');
+        $redis_conf = union_config('union.mid.checkLogin.redis',[]);
+        $user_name_conf = union_config('union.mid.checkLogin.user_name',[]);
+        $user_login_token = union_config('union.mid.checkLogin.user_token',[]);
 
-//        $z = $redis->set('z','20');
-        var_dump($value);
+        $redis = Redis::connection($redis_conf);
+
+        $inputs = request()->all();
+
+        if (array_key_exists($user_name_conf,$inputs) && array_key_exists($user_login_token,$inputs)){
+            $uid = $inputs[$user_name_conf];
+            $token = $inputs[$user_login_token];
+        }else{
+            return [
+                "code" => 500,
+                "detail" => "缺少参数",
+                "data"=>[]
+            ];
+        }
+
+        $redis_token = $redis->get($uid);
+        if ($token == $redis_token){
+            return '';
+        }else{
+            return [
+                "code" => 500,
+                "detail" => "请先登陆",
+                "data"=>[]
+            ];
+        }
     }
 }
